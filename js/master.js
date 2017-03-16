b5379956910fff2f37146e7e838d64395d33a6fc
Joe Romero <joethemovie@gmail.com>
2017-03-16 17:46:24 -0400

    tons of updates

--- js/master.js
+++ js/master.js
@@ -1,11 +1,29 @@
-
+$(document).ready(function() {
 window.onload = function() {
   const subArray = [`creating with <span class="redEmoji">&hearts;</span> since 1990`,`this is just a test`,`designer + developer` ];
 
   let arraySelector = Math.floor(Math.random() * 3 ) + 0;
-  console.log(arraySelector);
+
 
   document.getElementById('subtext').innerHTML = subArray[arraySelector];
   document.body.className += ' loaded'
 
 };
+let buttonTest = false;
+  $('#contactButton').click(function() {
+    if (buttonTest === false) {
+      buttonTest = true;
+      $('#additionalContact').css({'height' : '80px'}).delay(100);
+      $('#contactInformation').fadeIn(1000, function() {
+        $('#contactInformation').css({'display' : 'block'});
+      });
+    } else {
+      buttonTest = false;
+      $('#additionalContact').css({'height' : '0px'}).delay(100);
+      $('#contactInformation').fadeOut(100, function() {
+        $('#contactInformation').css({'display' : 'none'});
+      });
+    }
+
+  });
+});
